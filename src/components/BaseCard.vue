4dc9669b38f406053240531876f94b0b7966ca69
rcmaddox <65855506+rcmaddox@users.noreply.github.com>
2022-05-20 21:56:31 -0500

    Base Card for projects added.

--- src/components/BaseCard.vue
+++ src/components/BaseCard.vue
@@ -40,33 +40,39 @@ export default {
 <style scoped lang="scss">
 @import "@/assets/scss/main.scss";
 .project {
-  border: 1px solid;
+  border-radius: 10px;
   margin: 5rem 0;
   display: flex;
   justify-content: flex-start;
   align-items: center;
-  gap: 1rem;
-  padding: 1rem;
+  gap: 5rem;
+  padding: 5rem;
+  background-color: $color-background-secondary;
 }
 
 .project-image-front {
+  border-radius: 5px;
   width: 30rem;
   transition: all .3s ease;
-  border-radius: 5px;
   box-shadow: $box-shadow-small;
+  margin-bottom: 2rem;
 
   &:hover {
-  transform: scale(2.5) translateX(75px);
+  transform: scale(2) translateX(75px);
     box-shadow: $box-shadow-medium;
    }
 }
 
 .project-image-back {
+  border-radius: 5px;
   width: 30rem;
   transition: all .3s ease;
+  box-shadow: $box-shadow-small;
+  margin-bottom: 2rem;
+;
 
   &:hover {
-  transform: scale(2.5) translateX(-75px);
+  transform: scale(2) translateX(-75px);
     box-shadow: $box-shadow-medium;
    }
 }
@@ -74,6 +80,14 @@ export default {
 ul {
   list-style-type: none;
   font-size: $font-heading-5;
+  transition: all 0.3s ease;
 }
+
+  ul:hover {
+    transform: scale(1.5);
+    text-shadow: $box-shadow-small;
+    color: $color-secondary-font;
+  }
+
 
 </style>
